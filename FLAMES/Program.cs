7c32141127d97edf091c8ab99254ce331cf75940
hari <aravindhannew@gmail.com>
2023-07-02 14:40:30 +0530

    "console app complited"

--- FLAMES/Program.cs
+++ FLAMES/Program.cs
@@ -1,32 +1,64 @@
 ﻿
 using System.Text;
-var str = new StringBuilder();
 
+Thread.Sleep(1000);
+Console.Write(DateTime.Now);
+Console.Write("\nEnter the pin: ");
 var date = DateTime.Now;
+var str = new StringBuilder();
 str.Append(date.Month.ToString("00")); str.Append(date.Day.ToString("00")); str.Append(date.Year);
-Console.WriteLine(DateTime.Now);
-Console.Write("Enter the pin: ");
-
 
 string pin = Console.ReadLine();
 
-
-
-
-
-
-
-
 if (pin != "" && pin == str.ToString())
 {
-
+start:;
+    Console.Clear();
     Console.WriteLine("Welcome to the Flames game!");
+    Thread.Sleep(1000);
     Console.Write("Enter your name: ");
+    Thread.Sleep(1000);
     string yourName = Console.ReadLine();
+    Thread.Sleep(1000);
     Console.Write("Enter your partner's name: ");
+    Thread.Sleep(1000);
     string partnerName = Console.ReadLine();
+    if (yourName.Length == 0 || partnerName.Length == 0) return;
+    if (yourName.Contains("vijay") || partnerName.Contains("vijay")) { Console.Write("SISTER"); return; }
     string flamesResult = CalculateFlames(yourName, partnerName);
-    Console.WriteLine("Your Flames result is: " + flamesResult);
+
+    var result = string.Empty;
+    switch (flamesResult)
+    {
+
+        case "F": result = "FRIEND"; break;
+        case "L": result = "LOVER"; break;
+        case "A": result = "AFFECTION"; break;
+        case "M": result = "MARRIAGE"; break;
+        case "E": result = "ENEMY"; break;
+        case "S": result = "SISTER"; break;
+
+    }
+    Thread.Sleep(2000);
+    Console.Write("Processing");
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write(".");
+        Thread.Sleep(1000);
+    }
+    Console.WriteLine();
+    Console.WriteLine("++++++++++++++++++++++++++++++++");
+      
+    Console.WriteLine("+ Your Flames result is: " + result+" +");
+    Console.WriteLine("++++++++++++++++++++++++++++++++");
+    Console.WriteLine();
+    Console.WriteLine("Do you want to continue y/n");
+    var r = Console.ReadLine();
+
+    if (r.ToUpper() == "Y")
+        goto start;
+    else
+        return;
 }
 static string CalculateFlames(string yourName, string partnerName)
 {
@@ -46,6 +78,8 @@ static string CalculateFlames(string yourName, string partnerName)
             }
         }
     }
+
+
 
     int index = 0;
     for (int i = 1; i <= 5; i++)
